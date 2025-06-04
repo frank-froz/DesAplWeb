fcbb38bf2ac81450041ea3b29e97fb763bd3cab9
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-06-04 02:48:14 -0500

    Controlador: Integrado formato PDF/XLS y soporte de auditoría en CursoController

--- EC03-Huaytalla/PRJ_CRUD_SPRING_BOOT/src/main/java/com/tecsup/demo/controllers/CursoController.java
+++ EC03-Huaytalla/PRJ_CRUD_SPRING_BOOT/src/main/java/com/tecsup/demo/controllers/CursoController.java
@@ -1,4 +1,96 @@
 package com.tecsup.demo.controllers;
 
+import com.tecsup.demo.domain.entities.Curso;
+import com.tecsup.demo.services.CursoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import java.util.Map;
+
+@Controller
+@SessionAttributes("curso")
 public class CursoController {
+
+    @Autowired
+    CursoService cursoService;
+
+    @RequestMapping(value = "/listar", method = RequestMethod.GET)
+    public String listarCurso(Model model) {
+        model.addAttribute("cursos", cursoService.listar());
+        return "listar";
+    }
+
+    @RequestMapping(value = "/form")
+    public String crear(Map<String, Object> model) {
+
+        Curso curso = new Curso();
+        model.put("curso", curso);
+        return "form";
+    }
+
+    @RequestMapping(value = "/form", method = RequestMethod.POST)
+    public String guardar(@Valid Curso curso, BindingResult result, Model model, SessionStatus status) {
+        if(result.hasErrors()) {
+            return "form";
+        }
+
+        cursoService.grabar(curso);
+        status.setComplete();
+        return "redirect:listar";
+    }
+
+    //Form para editar
+    @RequestMapping(value="/form/{id}")
+    public String editar(@PathVariable(value="id") Integer id, Map<String, Object> model) {
+
+        Curso curso = null;
+
+        if(id > 0) {
+            curso = cursoService.buscar(id);
+        } else {
+            return "redirect:/listar";
+        }
+        model.put("curso", curso);
+        return "form";
+    }
+
+    //Eliminar
+    @RequestMapping(value="/eliminar/{id}")
+    public String eliminar(@PathVariable(value="id") Integer id) {
+
+        if(id > 0) {
+            cursoService.eliminar(id);
+        }
+        return "redirect:/listar";
+    }
+
+    //Ver
+    @RequestMapping(value="/ver")
+    public String ver(Model model) {
+        model.addAttribute("cursos", cursoService.listar());
+        model.addAttribute("titulo", "Lista de cursos");
+
+        return "curso/ver";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
