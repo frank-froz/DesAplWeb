dcd922a88ce6f50dbf60dcf63e62972c6a8a4591
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-06-04 02:49:30 -0500

    SQL: Carga inicial de datos en tabla cursos

---
+++ EC03-Huaytalla/PRJ_CRUD_SPRING_BOOT/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO cursos (nombre,creditos) VALUES('Programmer', 5);
+INSERT INTO cursos (nombre,creditos) VALUES('Developer', 5);
+INSERT INTO cursos (nombre,creditos) VALUES('Expert', 5);
+
+INSERT INTO alumnos (nombres, apellidos, fecha_nacimiento, sexo) VALUES('Franklin', 'Huaytalla', '1999-03-15', 'M');
+INSERT INTO alumnos (nombres, apellidos, fecha_nacimiento, sexo) VALUES('Hector', 'Perez', '1999-07-22', 'M');
+INSERT INTO alumnos (nombres, apellidos, fecha_nacimiento, sexo) VALUES('Ana', 'Ramirez', '2001-01-10', 'F');
+INSERT INTO alumnos (nombres, apellidos, fecha_nacimiento, sexo) VALUES('Hanmer', 'Castro', '1998-12-30', 'M');
+INSERT INTO alumnos (nombres, apellidos, fecha_nacimiento, sexo) VALUES('Luis', 'Gómez', '2002-04-05', 'M');
