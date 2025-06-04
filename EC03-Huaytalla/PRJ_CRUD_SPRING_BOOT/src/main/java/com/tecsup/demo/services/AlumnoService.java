9e536eac9046b7a14a51aef68be5bf0986e1a965
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-06-04 02:48:59 -0500

    Service: Interfaz AlumnoService con métodos necesarios

--- EC03-Huaytalla/PRJ_CRUD_SPRING_BOOT/src/main/java/com/tecsup/demo/services/AlumnoService.java
+++ EC03-Huaytalla/PRJ_CRUD_SPRING_BOOT/src/main/java/com/tecsup/demo/services/AlumnoService.java
@@ -1,4 +1,13 @@
 package com.tecsup.demo.services;
 
-public class AlumnoService {
+import com.tecsup.demo.domain.entities.Alumno;
+
+import java.util.List;
+
+public interface AlumnoService {
+
+    public void grabar(Alumno alumno);
+    public void eliminar(int id);
+    public Alumno buscar(Integer id);
+    public List<Alumno> listar();
 }
