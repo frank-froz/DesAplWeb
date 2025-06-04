340ee7c0801be4b02188ce07af5eea46eec22d17
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-06-04 02:48:44 -0500

    Entidad: Curso implementa AuditableEntity y mejoras generales

--- EC03-Huaytalla/PRJ_CRUD_SPRING_BOOT/src/main/java/com/tecsup/demo/domain/entities/Curso.java
+++ EC03-Huaytalla/PRJ_CRUD_SPRING_BOOT/src/main/java/com/tecsup/demo/domain/entities/Curso.java
@@ -1,4 +1,71 @@
 package com.tecsup.demo.domain.entities;
 
-public class Curso {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "cursos")
+public class Curso implements AuditableEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column
+    @NotEmpty
+    private String nombre;
+    @Column
+    @Max(5)
+    @Min(0)
+    private int creditos;
+
+    public Curso() {
+    }
+
+    public Curso(int id, String nombre, int creditos) {
+        this.id = id;
+        this.nombre = nombre;
+        this.creditos = creditos;
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    @Override
+    public String getNombreTabla() {
+        return "cursos";
+    }
+
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", creditos=" + creditos +
+                '}';
+    }
 }
