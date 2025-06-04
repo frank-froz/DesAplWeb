315cf7f5fd4f9b4fe1e90c1cf9eb7c66feb9f581
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-06-04 02:48:52 -0500

    DAO: AuditoriaDao para registrar eventos en base de datos

--- EC03-Huaytalla/PRJ_CRUD_SPRING_BOOT/src/main/java/com/tecsup/demo/domain/persistance/AuditoriaDao.java
+++ EC03-Huaytalla/PRJ_CRUD_SPRING_BOOT/src/main/java/com/tecsup/demo/domain/persistance/AuditoriaDao.java
@@ -1,4 +1,9 @@
-package com.tecsup.demo.domain.persistance;
+package com.tecsup.demo.domain.persistence;
 
-public interface AuditoriaDao {
+import com.tecsup.demo.domain.entities.Auditoria;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface AuditoriaDao extends CrudRepository<Auditoria, Integer> {
+
 }
