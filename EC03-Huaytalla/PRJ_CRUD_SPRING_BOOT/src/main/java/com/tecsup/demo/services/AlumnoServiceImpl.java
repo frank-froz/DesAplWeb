68b9eeb35b214cb237f08d0d46a843a65aabea0b
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-06-04 02:49:05 -0500

    Service: Implementación de AlumnoService con integración AOP

--- EC03-Huaytalla/PRJ_CRUD_SPRING_BOOT/src/main/java/com/tecsup/demo/services/AlumnoServiceImpl.java
+++ EC03-Huaytalla/PRJ_CRUD_SPRING_BOOT/src/main/java/com/tecsup/demo/services/AlumnoServiceImpl.java
@@ -1,4 +1,39 @@
 package com.tecsup.demo.services;
 
-public class AlumnoServiceImpl {
+import com.tecsup.demo.domain.entities.Alumno;
+import com.tecsup.demo.domain.persistance.AlumnoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class AlumnoServiceImpl implements AlumnoService {
+    @Autowired
+    private AlumnoDao dao;
+
+    @Override
+    @Transactional
+    public void grabar(Alumno alumno) {
+        dao.save(alumno);
+    }
+
+    @Override
+    @Transactional
+    public void eliminar(int id) {
+        dao.deleteById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Alumno buscar(Integer id) {
+        return dao.findById(id).orElse(null);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Alumno> listar() {
+        return (List<Alumno>) dao.findAll();
+    }
 }
