39090dcabe2f4e5ad7f97bd77c97fc9b92f7f4ad
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-07-02 18:37:45 -0500

    update

---
+++ S15/WS_SPRING_BOOT_ANGULAR/src/main/java/com/tecsup/demo/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.tecsup.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://react-springboot-empleados.vercel.app",
+                                "http://localhost:5173"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
--- S15/WS_SPRING_BOOT_ANGULAR/src/main/java/com/tecsup/demo/controlador/EmpleadoControlador.java
+++ S15/WS_SPRING_BOOT_ANGULAR/src/main/java/com/tecsup/demo/controlador/EmpleadoControlador.java
@@ -13,7 +13,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/")
-@CrossOrigin(origins = "*")
 public class EmpleadoControlador {
     @Autowired
     private EmpleadoRepositorio empleadoRepository;
