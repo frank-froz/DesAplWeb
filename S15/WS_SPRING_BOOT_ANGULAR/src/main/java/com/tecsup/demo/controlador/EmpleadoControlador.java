eaa74018e39eed62f9315957d9cc3d85de8239ec
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-07-02 18:24:47 -0500

    update

--- S15/WS_SPRING_BOOT_ANGULAR/src/main/java/com/tecsup/demo/controlador/EmpleadoControlador.java
+++ S15/WS_SPRING_BOOT_ANGULAR/src/main/java/com/tecsup/demo/controlador/EmpleadoControlador.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*s")
 public class EmpleadoControlador {
     @Autowired
     private EmpleadoRepositorio empleadoRepository;
